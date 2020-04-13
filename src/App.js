a22dcb6aaa1256606349863c74a5b01a9db108f1
kyle074613 <kylemj@uci.edu>
2020-04-12 20:46:55 -0700

    added contact page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import About from './pages/About'
+import Contact from './pages/Contact'
 
 function App() {
   return (
     <Router>
       <Header />
-      <Route exact path="/" component={About} />
+      <div className="container">
+        <Route exact path="/" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/portfolio" component={Contact} />
+      </div>
     </Router>
 
   );
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,2 +1,34 @@
 import React from 'react';
 
+function Contact() {
+    return (
+        <div className="row border content">
+
+            <h2>Contact Me</h2>
+
+            <div className='col-12'>
+                Name
+                <br />
+                <input className="nameinput w-100 my-1" type="text" name="FullName" placeholder="John Smith" />
+
+                <br />
+
+                Email
+                <br />
+                <input className="emailinput w-100 my-1" type="text" name="email" placeholder="example@gmail.com" />
+
+                <br />
+
+                Message
+                <br />
+                <textarea className="messageinput w-100 my-1" rows="10" cols="30" name="message"></textarea>
+
+                <br />
+
+                <input id="submitbutton" type="submit" name="Submit" />
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
