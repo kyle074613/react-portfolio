1e54f5716c8d066ebeee13161078238bc4f7ff7f
kyle074613 <kylemj@uci.edu>
2020-05-15 15:23:03 -0700

    updated portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Project from '../components/Project';
-import eatDaBurgerImage from '../assets/images/EatDaBurgerScreenshot.PNG';
+import SopanImage from "../assets/images/SoPan.PNG";
 import BudgetTrackerImage from '../assets/images/BudgetTracker.PNG';
 import FitnessTrackerImage from '../assets/images/FitnessTracker.PNG';
 import GoogleBooksSearchImage from '../assets/images/GoogleBooksSearch.PNG';
@@ -15,52 +15,52 @@ function Portfolio() {
                 <div className="row my-3">
                     <Project
                         key={0}
-                        name="Google Books Search"
-                        image={GoogleBooksSearchImage}
-                        site="https://glacial-temple-14829.herokuapp.com/"
-                        github="https://github.com/kyle074613/google-books-search"
+                        name="SoPan"
+                        image={SopanImage}
+                        site="https://so-pan.herokuapp.com/"
+                        github="https://github.com/hollyisaredhead/sopan"
                     />
                     <Project
                         key={1}
-                        name="Eat Da Burger"
-                        image={eatDaBurgerImage}
-                        site="https://blooming-escarpment-01798.herokuapp.com/"
-                        github="https://github.com/kyle074613/burger"
+                        name="Trip Buddy"
+                        image={Project1Image}
+                        site="https://tnowlan101.github.io/tnowlan101.github.io-Project1/"
+                        github="https://github.com/tnowlan101/tnowlan101.github.io-Project1"
                     />
                 </div>
 
                 <div className="row my-3">
                     <Project
                         key={2}
-                        name="Online/Offline Budget Tracker"
-                        image={BudgetTrackerImage}
-                        site="https://morning-chamber-52364.herokuapp.com/"
-                        github="https://github.com/kyle074613/online-offline-budget-tracker"
+                        name="Project X"
+                        image={Project2Image}
+                        site="https://sleepy-dusk-04483.herokuapp.com/html"
+                        github="https://github.com/hollyisaredhead/project2"
                     />
 
                     <Project
                         key={3}
-                        name="Workout Tracker"
-                        image={FitnessTrackerImage}
-                        site="https://workout-tracker-bootcamp-app.herokuapp.com/"
-                        github="https://github.com/kyle074613/workout-tracker"
+                        name="Google Books Search"
+                        image={GoogleBooksSearchImage}
+                        site="https://glacial-temple-14829.herokuapp.com/"
+                        github="https://github.com/kyle074613/google-books-search"
                     />
                 </div>
 
                 <div className="row my-3">
                     <Project
                         key={4}
-                        name="Group Project 2"
-                        image={Project2Image}
-                        site="https://sleepy-dusk-04483.herokuapp.com/html"
-                        github="https://github.com/hollyisaredhead/project2"
+                        name="Online/Offline Budget Tracker"
+                        image={BudgetTrackerImage}
+                        site="https://morning-chamber-52364.herokuapp.com/"
+                        github="https://github.com/kyle074613/online-offline-budget-tracker"
                     />
                     <Project
                         key={5}
-                        name="Group Project 1"
-                        image={Project1Image}
-                        site="https://tnowlan101.github.io/tnowlan101.github.io-Project1/"
-                        github="https://github.com/tnowlan101/tnowlan101.github.io-Project1"
+                        name="Workout Tracker"
+                        image={FitnessTrackerImage}
+                        site="https://workout-tracker-bootcamp-app.herokuapp.com/"
+                        github="https://github.com/kyle074613/workout-tracker"
                     />
                 </div>
             </div>
