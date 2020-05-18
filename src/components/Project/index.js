64b2d6fdbb2662ee7c6c2c25e6f703066b711d86
kyle074613 <kylemj@uci.edu>
2020-05-17 18:04:09 -0700

    updated portfolio cards

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,10 +3,11 @@ import React from 'react';
 function Project(props) {
     return (
         <div className="col-md-6 col-12">
-            <div className="card">
+            <div className="card h-100">
                 <img className="card-img-top" src={props.image} alt={props.name} />
                 <div className="card-body">
                     <h5 className="card-title">{props.name}</h5>
+                    <p class="card-text">{props.description}</p>
                     <a href={props.site} rel="noopener noreferrer" target="_blank" className="card-link">Go To App</a>
                     <a href={props.github} rel="noopener noreferrer" target="_blank" className="card-link">Github Repo</a>
                 </div>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -17,6 +17,7 @@ function Portfolio() {
                         key={0}
                         name="SoPan"
                         image={SopanImage}
+                        description="A place to hang out with your friends online. Users can watch youtube videos and chat together in real time."
                         site="https://so-pan.herokuapp.com/"
                         github="https://github.com/hollyisaredhead/sopan"
                     />
@@ -24,6 +25,7 @@ function Portfolio() {
                         key={1}
                         name="Trip Buddy"
                         image={Project1Image}
+                        description="Travel app to help users plan their trips. Gives user a map of a route along with driving directions, as well as restaurants and activities to check out at their destination."
                         site="https://tnowlan101.github.io/tnowlan101.github.io-Project1/"
                         github="https://github.com/tnowlan101/tnowlan101.github.io-Project1"
                     />
@@ -34,6 +36,7 @@ function Portfolio() {
                         key={2}
                         name="Project X"
                         image={Project2Image}
+                        description="A tool to help new web developers learn. Users have access to tutorial videos on web development languages, can ask questions, and look for solutions via Stack Overflow."
                         site="https://sleepy-dusk-04483.herokuapp.com/html"
                         github="https://github.com/hollyisaredhead/project2"
                     />
@@ -42,6 +45,7 @@ function Portfolio() {
                         key={3}
                         name="Google Books Search"
                         image={GoogleBooksSearchImage}
+                        description="Users can search for books via Google Books and save them to a list to look at later on Google Books. Created using React.js."
                         site="https://glacial-temple-14829.herokuapp.com/"
                         github="https://github.com/kyle074613/google-books-search"
                     />
@@ -52,6 +56,7 @@ function Portfolio() {
                         key={4}
                         name="Online/Offline Budget Tracker"
                         image={BudgetTrackerImage}
+                        description="Budget tracking application that can be used either online or offline. Offline activity is updated once the user goes back online."
                         site="https://morning-chamber-52364.herokuapp.com/"
                         github="https://github.com/kyle074613/online-offline-budget-tracker"
                     />
@@ -59,6 +64,7 @@ function Portfolio() {
                         key={5}
                         name="Workout Tracker"
                         image={FitnessTrackerImage}
+                        description="This website allows users keep track of workouts and total workout duration, distances, weight lifted, etc. Made with MongoDB."
                         site="https://workout-tracker-bootcamp-app.herokuapp.com/"
                         github="https://github.com/kyle074613/workout-tracker"
                     />
